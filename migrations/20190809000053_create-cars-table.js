7afee46d966fcd3d56416557aee2fc3ea99cf4e5
Ihab Ali <ihab@live.com>
2019-08-09 00:38:57 -0500

    add cars-schema to migrations file and command 'knex migrate:latest' to create db

--- migrations/20190809000053_create-cars-table.js
+++ migrations/20190809000053_create-cars-table.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex) {
-  
+  // don't forget the return statement
+  return knex.schema.createTable("cars", tbl => {
+    // creates a primary key called id
+    tbl.increments();
+    // creates a text field called vin which is both required and unique
+    tbl
+      .text("vin", 17)
+      .unique()
+      .notNullable();
+    // creates a numeric fields called make and model which is required
+    tbl.text("make", 64).notNullable();
+    tbl.text("model", 64).notNullable();
+    tbl.decimal("mileage", 10);
+  });
 };
 
 exports.down = function(knex) {
-  
+  // drops the entire table
+  return knex.schema.dropTableIfExists("cars");
 };
